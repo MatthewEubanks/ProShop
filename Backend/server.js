d76dcbfe7d7442367b0722763ca9d06d2ca27c88
MatthewEubanks <spc.eubanks.matthew@gmail.com>
2021-01-19 09:20:54 -0700

    second

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,31 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./Data/products");
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound, errorHandler } from "./Middleware/errorMiddleware.js";
+import connectDB from "./Config/db.js";
+
+import productRoutes from "./Routes/productRoutes.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("API is Running...");
+  res.send("API is Running...");
 });
 
-app.get("/api/products", (req, res) => {
-	res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
-	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`)
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`.yellow
+      .bold
+  )
 );
