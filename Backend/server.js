23aba37f448aa589e6ae974ac017072eea20a656
MatthewEubanks <spc.eubanks.matthew@gmail.com>
2021-01-22 12:02:14 -0700

    some stuff

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from "./Middleware/errorMiddleware.js";
 import connectDB from "./Config/db.js";
 
 import productRoutes from "./Routes/productRoutes.js";
+import userRoutes from "./Routes/userRoutes.js";
 
 dotenv.config();
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is Running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
