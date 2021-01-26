78bbb99e25c5b646bccfe944c90cd0a384c8cf46
MatthewEubanks <spc.eubanks.matthew@gmail.com>
2021-01-26 14:12:06 -0700

    adding

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../Models/userModel.js";
+import generateToken from "../Utils/generateToken.js";
 import Product from "../Models/userModel.js";
 
 //* @desc Auth user & get token
@@ -16,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -24,4 +25,45 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//* @desc  Register a new user
+//* @route POST /api/users
+//* @access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+//* @desc Get user profile
+//* @route GET /api/users/profile
+//* @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile };
--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../Controllers/userController.js";
+import { authUser, getUserProfile } from "../Controllers/userController.js";
+import { protect } from "../Middleware/authMiddleware.js";
 
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
