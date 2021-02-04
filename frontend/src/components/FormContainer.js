abc2b95b0011404f4dc695d82753555252de4ca2
MatthewEubanks <spc.eubanks.matthew@gmail.com>
2021-02-04 09:51:17 -0700

    stuff

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const FormContainer = () => {
+  return (
+    <Container>
+      <Row className=""></Row>
+    </Container>
+  );
+};
+
+export default FormContainer;
