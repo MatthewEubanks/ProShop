e08f31b98aef238d8f14ae5b11c0854ca310543c
MatthewEubanks <spc.eubanks.matthew@gmail.com>
2021-02-12 10:03:12 -0700

    user

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from "../Controllers/userController.js";
 import { protect } from "../Middleware/authMiddleware.js";
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
